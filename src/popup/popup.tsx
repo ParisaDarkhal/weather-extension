871bb962200aa72683d891272a78b183881c58fc
ParisaDarkhal <p.darkhal@gmail.com>
2024-09-19 12:40:14 -0400

    temp scale feature added

--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -3,17 +3,25 @@ import { createRoot } from 'react-dom/client'
 import './popup.css'
 import WeatherCard from './WheatherCard'
 import '@fontsource/roboto'
-import { Box, Paper, InputBase, IconButton } from '@mui/material'
+import { Box, Paper, InputBase, IconButton, Grid2 } from '@mui/material'
 import AddLocationIcon from '@mui/icons-material/AddLocation'
-import { setStoredCities, getStoredCities } from '../utils/storage'
+import {
+  setStoredCities,
+  getStoredCities,
+  setStoredOptions,
+  getStoredOption,
+  localStorageOptions,
+} from '../utils/storage'
 
 const App: React.FC<{}> = () => {
   const [cities, setCities] = useState<string[]>([])
   const [cityInput, setCityInput] = useState<string>('')
+  const [options, setOptions] = useState<localStorageOptions | null>(null)
 
   // Load stored cities when the component mounts
   useEffect(() => {
     getStoredCities().then((storedCities) => setCities(storedCities))
+    getStoredOption().then((options) => setOptions(options))
   }, [])
 
   // Function to update cities and save to storage
@@ -47,25 +55,53 @@ const App: React.FC<{}> = () => {
     updateCities(updatedCities)
   }
 
+  const handleTempScaleBtnClick = () => {
+    const updateOptions: localStorageOptions = {
+      ...options,
+      tempScale: options.tempScale === 'metric' ? 'imperial' : 'metric',
+    }
+    setStoredOptions(updateOptions).then(() => {
+      setOptions(updateOptions)
+    })
+  }
+
+  if (!options) {
+    return null
+  }
+
   return (
     <Box mx="5px" my="16">
-      <Paper>
-        <Box px="20px" py="5px">
-          <InputBase
-            placeholder="Add a city name"
-            value={cityInput}
-            onChange={(event) => setCityInput(event.target.value)}
-            onKeyDown={handleCityKeyPress}
-          />
-          <IconButton onClick={handleCityBtnClick}>
-            <AddLocationIcon />
-          </IconButton>
-        </Box>
-      </Paper>
+      <Grid2 container justifyContent={'space-evenly'}>
+        <Grid2>
+          <Paper>
+            <Box px="20px" py="5px">
+              <InputBase
+                placeholder="Add a city name"
+                value={cityInput}
+                onChange={(event) => setCityInput(event.target.value)}
+                onKeyDown={handleCityKeyPress}
+              />
+              <IconButton onClick={handleCityBtnClick}>
+                <AddLocationIcon />
+              </IconButton>
+            </Box>
+          </Paper>
+        </Grid2>
+        <Grid2>
+          <Paper>
+            <Box py="3px">
+              <IconButton onClick={handleTempScaleBtnClick}>
+                {options.tempScale === 'metric' ? '\u2103' : '\u2109'}
+              </IconButton>
+            </Box>
+          </Paper>
+        </Grid2>
+      </Grid2>
 
       {cities.map((city, index) => (
         <WeatherCard
           city={city}
+          tempScale={options.tempScale}
           key={index}
           onDelete={() => handleCityDeleteBtnClick(index)}
         />
--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -1,5 +1,11 @@
+import { OpenWeatherTempScale } from './api'
 export interface localStorage {
   cities?: string[]
+  options?: localStorageOptions
+}
+
+export interface localStorageOptions {
+  tempScale: OpenWeatherTempScale
 }
 
 export type LocalStorageKeys = keyof localStorage
@@ -20,6 +26,26 @@ export function getStoredCities(): Promise<string[]> {
   return new Promise((resolve) => {
     chrome.storage.local.get(keys, (res: localStorage) => {
       resolve(res.cities ?? [])
+    })
+  })
+}
+
+export function setStoredOptions(options: localStorageOptions): Promise<void> {
+  const vals: localStorage = {
+    options,
+  }
+  return new Promise((resolve) => {
+    chrome.storage.local.set(vals, () => {
+      resolve()
+    })
+  })
+}
+
+export function getStoredOption(): Promise<localStorageOptions> {
+  const keys: LocalStorageKeys[] = ['options']
+  return new Promise((resolve) => {
+    chrome.storage.local.get(keys, (res: localStorage) => {
+      resolve(res.options)
     })
   })
 }
