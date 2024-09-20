e27a172461d61fb15bed13f9623ef00b50234f8e
ParisaDarkhal <p.darkhal@gmail.com>
2024-09-20 13:46:01 -0400

    badge text added

--- src/background/background.ts
+++ src/background/background.ts
@@ -2,7 +2,10 @@ import {
   setStoredCities,
   getStoredCities,
   setStoredOptions,
+  getStoredOption,
 } from '../utils/storage'
+
+import { fetchOpenWeatherData } from '../utils/api'
 
 chrome.runtime.onInstalled.addListener(() => {
   setStoredCities([])
@@ -22,5 +25,24 @@ chrome.runtime.onInstalled.addListener(() => {
 chrome.contextMenus.onClicked.addListener((event) => {
   getStoredCities().then((cities) => {
     setStoredCities([...cities, event.selectionText])
+  })
+})
+
+chrome.alarms.create({
+  periodInMinutes: 60,
+})
+
+chrome.alarms.onAlarm.addListener(() => {
+  getStoredOption().then((options) => {
+    if (options.homeCity === '') {
+      return
+    }
+    fetchOpenWeatherData(options.homeCity, options.tempScale).then((data) => {
+      const temp = Math.round(data.main.temp)
+      const symbol = options.tempScale === 'metric' ? '\u2103' : '\u2109'
+      chrome.action.setBadgeText({
+        text: `${temp}${symbol}`,
+      })
+    })
   })
 })
